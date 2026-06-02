91eb92def63a017b0c4ec097ec12648bfbd17fca
EnfityBro <149163323+EnfityBro@users.noreply.github.com>
2026-06-02 21:58:56 +0300

    refactor: Following

--- Assets/Following.cs
+++ Assets/Following.cs
@@ -22,10 +22,6 @@ How to use:
 1. Attach this script to the game object that follows the target game object.
 
 
-Documentation:
-- The "target" is the object that the original game object will follow.
-
-
 Comment:
 - If you're sure about the target game object (that it's active and on scene all the time), you can remove the "if()" construct for more efficiency.
 - You can modify this script for your needs.
