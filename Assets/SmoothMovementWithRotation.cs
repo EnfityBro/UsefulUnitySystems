2240fd1df44aa627ef96581016f98c6ac532a363
EnfityBro <149163323+EnfityBro@users.noreply.github.com>
2026-06-02 23:54:26 +0300

    docs: update SmoothMovementWithRotation comments

--- Assets/SmoothMovementWithRotation.cs
+++ Assets/SmoothMovementWithRotation.cs
@@ -19,19 +19,19 @@ public class SmoothMovementWithRotation : MonoBehaviour
 
 How to use:
 1. Attach this script to a moving game object.
-2. In the "target" field in the "Inspector" window, assign the target game object Transform.
-3. Regulate the "moveSpeed" and "rotationSpeed" variables for your needs (you can also make these variables public).
+2. In the 'target' field in the Inspector window, assign the target game object Transform.
+3. Regulate the 'moveSpeed' and 'rotationSpeed' fields for your needs.
 
 
 Documentation:
-- The "target" variable is the game object (or point) that the original game object object will move to.
-- The "moveSpeed" variable is the movement speed of the original game object.
-- The "rotationSpeed" variable is the rotation speed of the original game object.
+- The 'target' field is the game object that the original game object object will move to.
+- The 'moveSpeed' field is the movement speed of the original game object.
+- The 'rotationSpeed' field is the rotation speed of the original game object.
 
 
 Comment:
-- This script allows you to create a flying game object form one point to another.
+- This script allows you to create a flying game object from one point to another.
   The original game object also performs a smooth rotation to the target point.
-- This approach ignores physical movement because movement is performed by "Vector3".
+- This approach ignores physical movement because movement is performed by Vector3.
 
 */
