7b6909764e705ca6bf5e4c23563900c170aafefa
EnfityBro <149163323+EnfityBro@users.noreply.github.com>
2026-06-02 20:51:17 +0300

    refactor: XRGrabInteractableExtra

--- Examples/XRGrabInteractableExtra.cs
+++ Examples/XRGrabInteractableExtra.cs
@@ -19,14 +19,18 @@ public class XRGrabInteractableExtra : XRGrabInteractable
 
     protected override void OnHoverEntered(HoverEnterEventArgs args)
     {
-        // do something when the player hovers the VR controller's ray at a game object
-        // use "args.interactorObject" to interact with the hovered object
+        // do something when the player hovers the VR controller's ray at the game object
+
+        // use "args.interactorObject" to get information about which VR controller's ray is hovered at the game object
+        // use "args.interactableObject" to interact with the hovered object
     }
 
     protected override void OnHoverExited(HoverExitEventArgs args)
     {
         // do something when the player moves the VR controller's ray away from the game object
-        // use "args.interactorObject" to interact with the hovered object
+
+        // use "args.interactorObject" to get information about which VR controller's ray is hovered at the game object
+        // use "args.interactableObject" to interact with the hovered object
     }
 }
 
@@ -35,7 +39,8 @@ public class XRGrabInteractableExtra : XRGrabInteractable
 /*
 
 How to use:
-1. Use this script instead of the original "XR Grab Interactable" component (attach this script to a game object that can be grabbed by the player).
+1. Use this override logic in your script inherited from the XRGrabInteractable class.
+2. Your script should be used instead of the original "XR Grab Interactable" component on a game object that can be grabbed by the player.
 
 
 Comment:
