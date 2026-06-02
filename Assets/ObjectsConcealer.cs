abd08e79c9f57f29ded5a3ebf8ae784aad6f217e
EnfityBro <149163323+EnfityBro@users.noreply.github.com>
2026-06-02 23:24:26 +0300

    refactor: NuclearOptimization

---
+++ Assets/ObjectsConcealer.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+public class ObjectsConcealer
+{
+    /// <summary>
+    /// Sets the rendering ability of a game object with a specified layer index.
+    /// </summary>
+    /// <remarks>
+    /// Don't call this method in Update or FixedUpdate base methods.
+    /// </remarks>
+    public static void SetRenderingForGameObjectsByLayerIndex(bool isEnabled, int layerIndex)
+    {
+        GameObject[] allGameObjects = GameObject.FindObjectsOfType<GameObject>();
+
+        foreach (GameObject currentGameObject in allGameObjects)
+        {
+            if (currentGameObject.layer == layerIndex)
+            {
+                try
+                {
+                    currentGameObject.GetComponent<MeshRenderer>().enabled = isEnabled;
+                }
+                catch (System.Exception) { }
+            }
+        }
+    }
+}
+
+
+
+/*
+
+How to use:
+1. Just copy this code into your project and call necessary methods.
+2. Create a layer that will mark the objects whose rendering you want to manage using the SetRenderingForGameObjectsByLayerIndex method.
+
+
+Comment:
+- This method enable or disable all mesh renderers, when it called.
+- This method is laborious and it's complexity is O(n), don't call it in Update or FixedUpdate base methods.
+- This method can optimize your GPU resources and raise FPS, but requires CPU and memory resources when it called.
+
+*/
