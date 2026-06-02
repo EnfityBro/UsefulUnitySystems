fc85eb48dd25cf57799615d2d9d417b4369f7f4f
EnfityBro <149163323+EnfityBro@users.noreply.github.com>
2026-06-03 00:05:38 +0300

    refactor: TryGetUniqueValue

--- Assets/TryGetUniqueValue.cs
+++ Assets/TryGetUniqueValue.cs
@@ -1,9 +1,5 @@
-using UnityEngine;
-
-public class TryGetUniqueValue : MonoBehaviour
+public static class TryGetUniqueValue
 {
-    private int previousRandomValue = -1;
-
     /// <summary>
     /// Tries to minimize duplication of a random int value from the range [minValue, maxValue).
     /// </summary>
@@ -30,16 +26,6 @@ public class TryGetUniqueValue : MonoBehaviour
 /*
 
 How to use:
-1. Just copy the "TryGetUniqueRandomValue" method and use it in your script (or attach this script to any game object).
-
-
-Documentation:
-- The "previousRandomValue" parameter is a reference to the previous random value. This variable controls the duplication.
-- The "minValue" parameter is the minimum value from a random range. This value is included in the range.
-- The "maxValue" parameter is the maximum value from a random range. This value is excluded from the range.
-
-
-Comment:
-- This method tries to minimize duplication of a random int value from the range [minValue, maxValue).
+1. Just copy this code into your project and call necessary methods.
 
 */
