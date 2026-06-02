3b1dc809dcfa8c70e8fc1d0fe1e1e8d58f59273f
EnfityBro <149163323+EnfityBro@users.noreply.github.com>
2026-06-03 00:08:34 +0300

    refactor: VRCameraOffsetFix

--- Assets/VRCameraOffsetFix.cs
+++ Assets/VRCameraOffsetFix.cs
@@ -3,7 +3,10 @@ using UnityEngine;
 
 public class VRCameraOffsetFix : MonoBehaviour
 {
-    private void Start() => StartCoroutine(CameraOffsetDelay());
+    private void Start()
+    {
+        StartCoroutine(CameraOffsetDelay());
+    }
 
     private IEnumerator CameraOffsetDelay()
     {
@@ -20,16 +23,16 @@ public class VRCameraOffsetFix : MonoBehaviour
 /*
 
 How to use:
-1. Select the "Not Specified" parameter in the "Tracking Origin Mode" field in the "XR Origin" component of the "XR Origin Rig" object.
-2. Set the "Camera Y Offset" parameter to 0 in the "XR Origin" component of the "XR Origin Rig" object.
-3. Attach this script to "Camera Offset" object in your "XR Origin Rig".
-4. Set the Y position to 0 for the "Camera Offset".
-5. Set the Y position to 0 for the "Main Camera".
-7. Manage the "PlayerPrefs.GetFloat("VRCameraOffsetValue")" value from another script (for example, from the game settings script).
+1. Select the 'Not Specified' parameter in the 'Tracking Origin Mode' field in the 'XR Origin' component of the 'XR Origin Rig' object.
+2. Set the 'Camera Y Offset' parameter to 0 in the 'XR Origin' component of the 'XR Origin Rig' object.
+3. Attach this script to 'Camera Offset' object in your 'XR Origin Rig'.
+4. Set the Y position to 0 for the 'Camera Offset'.
+5. Set the Y position to 0 for the 'Main Camera'.
+7. Manage the 'PlayerPrefs.GetFloat("VRCameraOffsetValue")' value from another script (for example, from the game settings script).
 
 
 Comment:
-- Instead of PlayerPrefs you can use another save/load system or use your fixed value.
+- Instead of PlayerPrefs you can use another save/load system (see also InternalParams: https://github.com/EnfityBro/InternalParams) or use your fixed value.
 - This script allows you to quickly solve the problem with the incorrect VR camera offset in the game build.
   This problem was noticed on Oculus Quest 2 due to incorrect settings of the play zone.
   This script will be useless if you select other settings of the "Tracking Origin Mode" field,
